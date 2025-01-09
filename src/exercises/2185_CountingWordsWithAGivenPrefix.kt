07b46a19cf55abab0379cb30ec1b7bc10f83f5cd
julianoccm <julianocolerecmoreira@gmail.com>
2025-01-09 13:43:44 -0300

    feat: 2185

---
+++ src/exercises/2185_CountingWordsWithAGivenPrefix.kt
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/counting-words-with-a-given-prefix/
+
+package exercises
+
+class `2185_CountingWordsWithAGivenPrefix` {
+    fun prefixCount(words: Array<String>, pref: String): Int {
+        var counter = 0
+
+        for (word in words) {
+            if(word.startsWith(pref)) {
+                counter++
+            }
+        }
+
+        return counter
+    }
+}
