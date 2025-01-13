e196b530f3d0fd6c6937adc0921e0b3a0a130821
Juliano Colere Ceccon Moreira <66191563+julianoccm@users.noreply.github.com>
2025-01-13 20:15:29 -0300

    feat: 13

---
+++ solutions/RomanToInteger.kt
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/roman-to-integer
+
+class RomanToInteger {
+    fun romanToInt(s: String): Int {
+        val romanToNumber = mapOf(
+            "I" to 1,
+            "V" to 5,
+            "X" to 10,
+            "L" to 50,
+            "C" to 100,
+            "D" to 500,
+            "M" to 1000
+        )
+
+        val simplifiedString = s
+            .replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC")
+        
+        var sum = 0
+        for (char in simplifiedString)
+            sum += romanToNumber.getValue(char.toString())
+
+        return sum
+    }
+}
